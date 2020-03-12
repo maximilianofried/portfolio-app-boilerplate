c6db4f966cbaa7543e5add9317651586b51c623f
Maxi <maximiliano.fried@gmail.com>
2020-03-12 13:43:15 +0100

    target blank sn icons

--- pages/blogs.js
+++ pages/blogs.js
@@ -76,7 +76,7 @@ class Blog extends React.Component {
                     <div className="col-lg-8 col-md-10 mx-auto">
                         <ul className="list-inline text-center">
                         <li className="list-inline-item">
-                            <a href="#">
+                            <a target="_blank" href="#">
                             <span className="fa-stack fa-lg">
                                 <i className="fas fa-circle fa-stack-2x"></i>
                                 <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
@@ -84,7 +84,7 @@ class Blog extends React.Component {
                             </a>
                         </li>
                         <li className="list-inline-item">
-                            <a href="#">
+                            <a target="_blank" href="#">
                             <span className="fa-stack fa-lg">
                                 <i className="fas fa-circle fa-stack-2x"></i>
                                 <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
@@ -92,7 +92,7 @@ class Blog extends React.Component {
                             </a>
                         </li>
                         <li className="list-inline-item">
-                            <a href="#">
+                            <a target="_blank" href="#">
                             <span className="fa-stack fa-lg">
                                 <i className="fas fa-circle fa-stack-2x"></i>
                                 <i className="fab fa-github fa-stack-1x fa-inverse"></i>
