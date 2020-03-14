c7575c39f778d129242062886549382104646417
maximilianofried <maximiliano.fried@gmail.com>
2020-03-14 12:41:42 +0100

    solves heading error

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -3,8 +3,9 @@ import Header from '../shared/Header';
 import Head from 'next/head';
 
 const BaseLayout = (props) => {
-    const { className, children, isAuthenticated, user, isSiteOwner, title, cannonical } = props;
+    const { className, children, isAuthenticated, user, isSiteOwner, cannonical } = props;
     const headerType = props.headerType || 'defualt';
+    const title = props.title || 'Fried Maximiliano Web';
     return (
         <React.Fragment>
             <Head>
