029c8a53e430ee945e7d57585be41dbd66ff3f05
maximilianofried <maximiliano.fried@gmail.com>
2020-03-14 12:23:51 +0100

    fixes env variables

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DB_URI: 'mongodb+srv://dbUser2:dbPassword2@cluster0-jc8m1.mongodb.net/test?retryWrites=true&w=majority'
+    DB_URI: 'mongodb+srv://dbUser2:dbPassword2@cluster0-jc8m1.mongodb.net/test?retryWrites=true&w=majority',
+    NAMESPACE: 'https://portfolio-app-boilerplate.herokuapp.com'
 }
